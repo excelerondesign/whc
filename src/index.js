7a0a1d088c9715933489d3d2c5d067f9c1c9eb85
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-06-25 12:47:30 -0400

    🎨 updates createWorker function

--- src/index.js
+++ src/index.js
@@ -4,57 +4,87 @@
  */
 import worker from './includes/worker';
 (function () {
-    'use strict';
+    const script = document.getElementById("whcScriptTag");
 
-    var Constructor = function () {
+    const forms = [...document.getElementsByClassName(script.dataset.form)];
 
+    var Constructor = function (form, index) {
         const Private = {};
+
+        // now converted to seconds
         Private.time = Math.floor(Date.now() / 1000);
-        Private.script = document.getElementById('whcScriptTag');
-        Private.form = document.getElementById(Private.script.dataset.form);
-        Private.button = document.getElementById(Private.script.dataset.button);
-        Private.difficulty = Private.button.dataset.difficulty || 5;
-        Private.eventName = 'WHC::Message -> ';
 
-        Private.workerFunc = worker;
+        // current time + 1 hour;
+        Private.ttl = Private.time + 3600;
+        //// Checks if there is a form or if there is an
+        //// Private.form = document.getElementById(script.dataset.form);
+        // use a unique class selector for the forms
+        Private.form = form;
+        Private.ID = Private.form.getAttribute("id") || "Form " + index;
+        // should be a class selector
+        // each button should also have a 'data-finished' text that the button should end on
+        Private.button = Private.form.getElementsByClassName(
+            script.dataset.button
+        )[0];
+        // find the average difficulty of all the buttons on the page or use five
 
-        // converts the debug value into a boolean, 
+        Private.difficulty = parseInt(Private.button.dataset.difficulty) || 5;
+
+        Private.eventName = "WHC|" + Private.ID + "::Message -> ";
+
+        // converts the debug value into a boolean,
         // so truthy becomes Boolean true, and Falsy becomes Boolean false
         // (https://developer.mozilla.org/en-US/docs/Glossary/Truthy - https://developer.mozilla.org/en-US/docs/Glossary/Falsy)
-        Private.debug = Boolean(Private.form.dataset.whcDebug);
-        window.addEventListener(Private.eventName, ({ detail }) => console.log(Private.eventName + detail), false);
+        // checks all the forms to see if any of them have the debug flag, and then checks if it is true
+        Private.debug =
+            "debug" in Private.form.dataset && Boolean(Private.form.dataset.debug);
+
+        if (Private.debug) {
+            localStorage.removeItem("WHCStorage");
+            window.WHCDetails = window.WHCDetails || [];
+            window.WHCDetails.push({
+                form,
+                button: Private.button,
+                difficulty: Private.difficulty
+            });
+            window.addEventListener(
+                Private.eventName,
+                ({ detail }) => console.log(Private.eventName + detail),
+                false
+            );
+        }
 
         var emit = function (detail) {
             if (!Private.debug) return;
             window.dispatchEvent(new CustomEvent(Private.eventName, { detail }));
         };
 
-        emit('Constructing');
+        emit("Constructing");
 
-        var publicAPIs = {};
+        // var publicAPIs = {};
 
-        var enableButton = function () {
-            Private.button.classList.add('done');
-            Private.button.disabled = false;
-            Private.button.value = Private.button.dataset.finished;
+        var enableButton = function (button) {
+            button.classList.add("done");
+            button.disabled = false;
+            button.value = button.dataset.finished;
         };
 
         var createWorker = function () {
-            var worker = null;
+            var employee = null;
             try {
                 var blob = new Blob(
                     // generates a worker by converting  into a string and then running that function as a worker
-                    ['(' + Private.workerFunc.toString() + ')();'], { type: 'application/javascript' });
+                    ['(' + worker.toString() + ')();'], { type: 'application/javascript' });
                 var url = window.URL || window.webkitURL;
                 var blobUrl = url.createObjectURL(blob);
-                worker = new Worker(blobUrl);
+                employee = new Worker(blobUrl);
             } catch (e1) {
                 emit('createWorker: Worker Error');
                 //if it still fails, there is nothing much we can do
                 console.error(e1);
             }
             emit('createWorker: Worker Created');
-            return worker;
+            return employee;
         };
 
         // function(){importScripts("https://wehatecaptchas.com/sha256.js");const getWholePercent=(e,i)=>Math.floor(e/i*100),isPrime=e=>{for(var i=2;i<e;i++)if(e%i==0)return!1;return e>1},solveCaptcha=(e,i=1)=>{i++;for(var s={question:e.question,time:e.time,nonce:i},t=JSON.stringify(s),a=sha256(t);"0000"!==a.substr(0,4)||!isPrime(i);){i++;s={question:e.question,time:e.time,nonce:i},t=JSON.stringify(s),a=sha256(t)}return{verify_array:s,nonce:i,hash:a}};var verification=[];if(null===nonce)var nonce=1;self.addEventListener("message",(function(e){var i=e.data;self.postMessage({action:"message",message:"Checking if you're a bot before enabling submit button..."});for(var s,t,a=i.difficulty,n=0;n<a;n++){var r=solveCaptcha(i,o),o=r.nonce;verification.push(r.verify_array);var c=(s=n+1,t=a,Math.floor(s/t*100));self.postMessage({action:"message",message:`Still checking... ${c}% done`})}self.postMessage({action:"captchaSuccess",verification:verification}),verification=[],o=1}),!1);}
