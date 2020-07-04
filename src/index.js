c0c1d079307a69be3e55959259241c9c3bcf2b36
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-07-04 13:15:31 -0400

    edits the prettier config

--- src/index.js
+++ src/index.js
@@ -28,8 +28,8 @@
  * @property {Verification[]} verification
  */
 
-import worker from './includes/worker'
-;(function () {
+import worker from './includes/worker';
+(function () {
 	/**
 	 * @type {whcOptions} whcDefaults
 	 */
@@ -39,29 +39,29 @@ import worker from './includes/worker'
 		debug: false,
 		difficulty: 3,
 		finished: 'Submit',
-	}
+	};
 
 	/**
 	 * @type {whcOptions} window.whcConfig
 	 */
-	var whcConfig = Object.assign(whcDefaults, window.whcConfig ?? {})
+	var whcConfig = Object.assign(whcDefaults, window.whcConfig ?? {});
 
 	/**
 	 * @type {NodeListOf<HTMLFormElement>} forms
 	 */
-	var forms = document.querySelectorAll(whcConfig.form)
+	var forms = document.querySelectorAll(whcConfig.form);
 
 	/**
 	 * @param {string} str
 	 */
 	var parse = function (str) {
-		var num = parseInt(str)
+		var num = parseInt(str);
 
-		if (isNaN(num)) return false
-		if (num !== num) return false
+		if (isNaN(num)) return false;
+		if (num !== num) return false;
 
-		return num
-	}
+		return num;
+	};
 
 	/**
 	 *
@@ -69,113 +69,115 @@ import worker from './includes/worker'
 	 * @param {number} index
 	 */
 	var Constructor = function (form, index) {
-		var Private = {}
+		var Private = {};
 
 		/**
 		 * @type {number} Now converted to seconds
 		 */
-		Private.time = Math.floor(Date.now() / 1000)
+		Private.time = Math.floor(Date.now() / 1000);
 
 		/**
 		 * @type {HTMLFormElement}
 		 */
-		Private.form = form
+		Private.form = form;
 
 		/**
 		 * @type {string}
 		 */
-		Private.ID = form.getAttribute('id') || 'Form ' + index
+		Private.ID = form.getAttribute('id') || 'Form ' + index;
 
 		/**
 		 * @type {HTMLButtonElement}
 		 */
-		Private.button = form.querySelector(whcConfig.button)
+		Private.button = form.querySelector(whcConfig.button);
 
 		/**
 		 * @type {number}
 		 */
 		Private.difficulty =
 			parse(Private.button.getAttribute('data-difficulty')) ||
-			whcConfig.difficulty
+			whcConfig.difficulty;
 
 		/**
 		 * @type {string}
 		 */
-		Private.eventName = 'WHC|' + Private.ID
+		Private.eventName = 'WHC|' + Private.ID;
 
 		Private.debug =
 			'debug' in form.dataset
 				? Boolean(form.dataset.debug)
-				: whcConfig.debug
+				: whcConfig.debug;
 
 		if (Private.debug) {
-			window.whcDetails || []
+			window.whcDetails || [];
 			window.whcDetails.push({
 				form: Private.form,
 				button: Private.button,
 				difficulty: Private.difficulty,
-			})
+			});
 			window.addEventListener(
 				Private.eventName,
 				({ detail }) =>
 					console.log(Private.eventName + '::Message -> ' + detail),
 				false
-			)
+			);
 		}
 
 		/**
 		 * @param {string} detail
 		 */
 		var emit = function (detail) {
-			if (!whcConfig.debug) return
-			window.dispatchEvent(new CustomEvent(Private.eventName, { detail }))
-		}
+			if (!whcConfig.debug) return;
+			window.dispatchEvent(
+				new CustomEvent(Private.eventName, { detail })
+			);
+		};
 
-		emit('Constructing')
-		console.log(emit)
+		emit('Constructing');
+		console.log(emit);
 		/**
 		 * @param {HTMLButtonElement} button
 		 */
 		var enableButton = function (button) {
-			var { finished } = button.dataset
-			button.classList.add('done')
-			button.removeAttribute('disabled')
-			button.setAttribute('value', finished)
-		}
+			var { finished } = button.dataset;
+			button.classList.add('done');
+			button.removeAttribute('disabled');
+			button.setAttribute('value', finished);
+		};
 
 		var createWorker = function () {
-			emit('createWorker(): Creating')
+			emit('createWorker(): Creating');
 			try {
 				// generates a worker by converting  into a string and then running that function as a worker
 				var blob = new Blob(['(' + worker.toString() + ')();'], {
 					type: 'application/javascript',
-				})
-				var blobUrl = URL.createObjectURL(blob)
-				var laborer = new Worker(blobUrl)
-				emit('createWorker(): Created')
+				});
+				var blobUrl = URL.createObjectURL(blob);
+				var laborer = new Worker(blobUrl);
+				emit('createWorker(): Created');
 
-				return laborer
+				return laborer;
 			} catch (e1) {
-				emit('createWorker(): Error')
+				emit('createWorker(): Error');
 				//if it still fails, there is nothing much we can do
-				throw new Error('Uknown Error: ' + e1)
+				throw new Error('Uknown Error: ' + e1);
 			}
-		}
+		};
 
-		Private.worker = createWorker()
+		Private.worker = createWorker();
 
 		var beginVerification = function () {
-			var { difficulty, time, worker } = Private
+			var { difficulty, time, worker } = Private;
 
-			emit('Difficulty Level: ' + difficulty)
+			emit('Difficulty Level: ' + difficulty);
 
 			worker.postMessage({
 				difficulty,
 				time,
-			})
+			});
 
-			emit('Verification: Message Sent')
-		}
+			emit('Verification: Message Sent');
+		};
 
 		/**
 		 *
@@ -183,12 +185,12 @@ import worker from './includes/worker'
 		 * @param {Verification} verification
 		 */
 		var addVerification = function (form, verification) {
-			var input = document.createElement('input')
-			input.setAttribute('type', 'hidden')
-			input.setAttribute('name', 'captcha_verification')
-			input.setAttribute('value', JSON.stringify(verification))
-			form.appendChild(input)
-		}
+			var input = document.createElement('input');
+			input.setAttribute('type', 'hidden');
+			input.setAttribute('name', 'captcha_verification');
+			input.setAttribute('value', JSON.stringify(verification));
+			form.appendChild(input);
+		};
 
 		/**
 		 *
@@ -196,12 +198,12 @@ import worker from './includes/worker'
 		 * @param {string} string
 		 */
 		var updatePercent = function (button, string) {
-			var percent = string.match(/\d*%/)
-			if (percent === null) return
+			var percent = string.match(/\d*%/);
+			if (percent === null) return;
 
-			button.setAttribute('data-progress', percent)
-			emit('Verification Progress: ' + percent)
-		}
+			button.setAttribute('data-progress', percent);
+			emit('Verification Progress: ' + percent);
+		};
 
 		/**
 		 *
@@ -210,27 +212,27 @@ import worker from './includes/worker'
 		 */
 		var workerMessageHandler = function ({ data }) {
 			if (data.action === 'captchaSuccess') {
-				addVerification(Private.form, data.verification)
-				enableButton(Private.button)
-				emit('Verification Progress: Complete')
+				addVerification(Private.form, data.verification);
+				enableButton(Private.button);
+				emit('Verification Progress: Complete');
 
-				return
+				return;
 			} else if (data.action === 'message') {
-				updatePercent(Private.button, data.message)
-				return
+				updatePercent(Private.button, data.message);
+				return;
 			}
-			emit('Message Handler: ERROR - UNKNOWN')
-		}
+			emit('Message Handler: ERROR - UNKNOWN');
+		};
 
 		window.addEventListener('load', beginVerification, {
 			once: true,
 			capture: true,
-		})
+		});
 
-		Private.worker.addEventListener('message', workerMessageHandler, false)
+		Private.worker.addEventListener('message', workerMessageHandler, false);
 
-		emit('Constructed')
-	}
+		emit('Constructed');
+	};
 
-	forms.forEach((form, i) => new Constructor(form, i))
-})()
+	forms.forEach((form, i) => new Constructor(form, i));
+})();
