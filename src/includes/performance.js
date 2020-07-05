8ee725ee29ad56e0861300ad49db25606acef981
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-07-05 18:58:13 -0400

    🎨 adds index param to pComplete()
    📝 makes start param options on p()

--- src/includes/performance.js
+++ src/includes/performance.js
@@ -2,15 +2,17 @@
  *
  * @param {string} name - Performance mark/measure name
  * @param {string} method - 'mark'/'measure'
- * @param {string} start - Name of the performance mark to measure based on
+ * @param {string} [start] - Name of the performance mark to measure based on
  */
 export default function p(name, method, start) {
 	performance[method](name, start);
 }
 
-function pComplete() {
+function pComplete(index = 0) {
 	return performance
 		.getEntries()
-		.filter(entry => entry.name.includes('whc:'));
+		.filter(
+			entry => entry.name.includes('whc:') && entry.name.includes(index)
+		);
 }
 export { pComplete };
