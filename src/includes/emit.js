186b0693c0773cdf0041f986ce108ea2fc92ce01
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-07-04 21:56:08 -0400

    📝 adds JSDoc comments

--- src/includes/emit.js
+++ src/includes/emit.js
@@ -1,7 +1,12 @@
-export default function (element, eventType, detail = {}) {
+/**
+ * @param {HTMLElement} element
+ * @param {string} eventType
+ * @param {WHCEventDetail} detail
+ */
+export default function (element, eventType, detail) {
 	var event = new CustomEvent(eventType, {
 		bubbles: true,
-		detail,
+		detail: detail || {},
 	});
 
 	element.dispatchEvent(event);
