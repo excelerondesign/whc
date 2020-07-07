fe029edc8653a63deeb2f099633bd1ee3e57a080
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-07-06 20:06:56 -0400

    🎨 adds defaults to event object
    🎨 moves perf calls to event call
    
    💄 imports typedefs instead of redefining

--- src/includes/emit.js
+++ src/includes/emit.js
@@ -1,9 +1,4 @@
-/**
- * @typedef {Object} Verification
- * @prop {number} nonce
- * @prop {number} time
- * @prop {string} question
- */
+import p from './performance';
 
 /**
  * @typedef {Object} WHCEventDetail
@@ -12,7 +7,7 @@
  * @prop {boolean} complete
  * @prop {number} [progress]
  * @prop {string} [error]
- * @prop {Verification[]} [verification]
+ * @prop {import('./worker.js').Verification[]} [verification]
  * @prop {string} emoji
  */
 
@@ -20,16 +15,25 @@
  * @param {HTMLFormElement} form
  * @param {string} eventType
  * @param {WHCEventDetail} detail
+ * @param {import('./performance.js').Perf[]} objects
  */
-export default function (form, eventType, detail) {
+export default function (form, eventType, detail, perf, ...objects) {
 	var defaultDetail = {
 		form,
 		time: Date.now(),
+		done: false,
+		verification: [],
+		progress: 0,
 	};
+
 	var event = new CustomEvent(eventType, {
 		bubbles: true,
 		detail: Object.assign(defaultDetail, detail || {}),
 	});
 
 	form.dispatchEvent(event);
+
+	if (perf && objects !== null) {
+		objects.forEach(p); // if performance objects are passed, run the perf function
+	}
 }
