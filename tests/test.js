43505b5ba3f967a9a5712030a9d66d8652460af1
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-07-06 22:09:16 -0400

    changes performance setting name to perf

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,6 @@
 window.whcConfig = window.whcConfig || {};
 window.whcConfig.events = true;
-window.whcConfig.performance = true;
+window.whcConfig.perf = true;
 document.addEventListener('whc:Start', ({ detail }) => console.log(detail));
 
 document.addEventListener('whc:Update', ({ detail }) => console.log(detail));
