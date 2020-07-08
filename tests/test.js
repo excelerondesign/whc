b733ca54142e64a7aa376afa4c0c5305b5683a65
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-07-08 13:15:36 -0400

    changes some of the test JS

--- tests/test.js
+++ tests/test.js
@@ -1,11 +1,19 @@
 window.whcConfig = window.whcConfig || {};
 window.whcConfig.events = true;
 window.whcConfig.perf = true;
-document.addEventListener('whc:Start', ({ detail }) => console.log(detail));
+window.addEventListener('whc:Start#2', event =>
+	console.log(event.type, event.detail)
+);
 
+/*
 document.addEventListener('whc:Update', ({ detail }) => console.log(detail));
 
 document.addEventListener('whc:Complete', ({ detail }) => {
 	detail.form.classList.add('complete');
 	console.log(detail);
 });
+*/
+
+document
+	.querySelectorAll('form')[0]
+	.addEventListener('whc:Progress#0', console.log);
